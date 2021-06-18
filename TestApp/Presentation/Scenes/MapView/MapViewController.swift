f82c12faacb58d0846a3b97d6fc1e84206f808b9
Sho <developer@shota-ito.com>
2021-06-18 23:53:51 +0900

    :sparkles: create MapView

---
+++ TestApp/Presentation/Scenes/MapView/MapViewController.swift
@@ -0,0 +1,31 @@
+//
+//  MapViewController.swift
+//  TestApp
+//
+//  Created by Shota Ito on 2021/06/18.
+//
+
+import UIKit
+
+final class MapViewController: UIViewController {
+
+    @IBOutlet weak var mapView: MKMapView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
